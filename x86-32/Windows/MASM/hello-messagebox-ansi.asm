7f0db644fb7a688ec2d315ad6c593c24e68fd00b
veganaize <veganaize@users.noreply.github.com>
2021-08-13 16:03:24 -0700

    Add hello-messagebox-ansi x86-32/windows/masm

---
+++ x86-32/Windows/MASM/hello-messagebox-ansi.asm
@@ -0,0 +1,28 @@
+.386
+.model flat, stdcall
+option casemap: none
+
+includelib kernel32.lib
+    ExitProcess PROTO :DWORD
+
+includelib user32.lib
+    MessageBoxA PROTO :DWORD, :DWORD, :DWORD, :DWORD
+
+
+.const
+    sGREETING byte "Hello MessageBox"
+
+
+.code
+start:
+
+    push 0      ;MB_OK
+    push offset sGREETING
+    push offset sGREETING
+    push 0      ;NULL
+    call MessageBoxA
+
+    push 0
+    call ExitProcess
+
+end start
