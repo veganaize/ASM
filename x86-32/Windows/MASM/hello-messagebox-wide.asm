f762d15c03d6c88a13ecf6d8c2a8bc91fdddfa9b
veganaize <veganaize@users.noreply.github.com>
2021-08-13 16:29:23 -0700

    Add hello-messagebox-wide x86-32/windows/masm

---
+++ x86-32/Windows/MASM/hello-messagebox-wide.asm
@@ -0,0 +1,28 @@
+.386
+.model flat, stdcall
+option casemap: none
+
+includelib kernel32.lib
+    ExitProcess PROTO :DWORD
+
+includelib user32.lib
+    MessageBoxW PROTO :DWORD, :DWORD, :DWORD, :DWORD
+
+
+.const
+    wsGREETING word 'H','e','l','l','o',' ','M','e','s','s','a','g','e','B','o','x'
+
+
+.code
+start:
+
+    push 0      ;MB_OK
+    push offset wsGREETING
+    push offset wsGREETING
+    push 0      ;NULL
+    call MessageBoxW
+
+    push 0
+    call ExitProcess
+
+end start
